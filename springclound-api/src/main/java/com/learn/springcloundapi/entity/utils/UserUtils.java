3ed8bc9551de25b20686d120f425ff01ea927add
ShiAsawei <1714316219@qq.com>
2019-06-07 14:14:18 +0800

    update utils

--- springclound-api/src/main/java/com/learn/springcloundapi/entity/utils/UserUtils.java
+++ springclound-api/src/main/java/com/learn/springcloundapi/entity/utils/UserUtils.java
@@ -10,15 +10,17 @@ public class UserUtils {
     static   UserInfo user =new UserInfo();
 
     public static List<UserInfo> userInfoList(){
+        hello();
 
+        return li;
+    }
 
+    public static void hello(){
         for (int i = 0; i < 5; i++) {
             user.setId(i+1);
             user.setUsername("zhangSan"+i);
             user.setPassword("1234"+i);
             li.add(user);
         }
-
-        return li;
     }
 }
